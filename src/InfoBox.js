91aa99ab98385ab9509760418563ea330bd42ea4
anikachoudhury <anikachoudhury010@gmail.com>
2021-05-24 14:06:52 +0100

    updated app.js and app.css

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Card, CardContent, Typography } from "@material-ui/core";
+import './InfoBox.css';
 
 
 function InfoBox({ title, cases, total }) {
     return (
         <div className="infoBox">
-            <Card>
-                <CardContent>
+            <Card className="infoBox__card">
+                <CardContent className="infoBox__content">
 
                     <Typography className="infoBox__title" color="textSecondary">
                         {title}
